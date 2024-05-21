93488bb33e88a6abefa15f1f35d318a77b5ff4cb
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-05-21 23:02:40 +0300

    init

---
+++ SnapShop/SnapShop/SnapShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapShopApp.swift
+//  SnapShop
+//
+//  Created by Abdullah Essam on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnapShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
