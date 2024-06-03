0f69cff67006ddb3cdfab9cfd27228ccdf9552ce
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-06-03 03:39:32 +0300

    feat : Auth Models

---
+++ SnapShop/SnapShop/Models/AddressModel.swift
@@ -0,0 +1,18 @@
+//
+//  AddressModel.swift
+//  SnapShop
+//
+//  Created by Abdullah Essam on 02/06/2024.
+//
+
+import Foundation
+
+//struct Address: Codable {
+//
+//    var address1 :String
+//    var city :String
+//    var country :String
+//
+//
+//
+//}
