aa9b0b5dfc3653e487071b323c8c2f7d20ce1121
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-06-06 14:58:56 +0300

    fix : Clear root

--- SnapShop/Pods/Target Support Files/Pods-SnapShop-SnapShopUITests/Pods-SnapShop-SnapShopUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SnapShop_SnapShopUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SnapShop_SnapShopUITests
-@end
--- SnapShop/Pods/Target Support Files/Pods-SnapShop/Pods-SnapShop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SnapShop : NSObject
-@end
-@implementation PodsDummy_Pods_SnapShop
-@end
